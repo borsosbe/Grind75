cbebff40de7a56873aa01ba3d81506ec087aa2ae
borsosbe <borsosbe@gmail.com>
2023-01-08 16:16:19 +0100

    Solved Two Sum problem
    https://leetcode.com/problems/two-sum/description/

--- Grind75/main.swift
+++ Grind75/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
+class Solution {
+    // 1. Two Sum https://leetcode.com/problems/two-sum/description/
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int: Int]()
+        for (i, num) in nums.enumerated() {
+            if let index = dict[target-num] {
+                if i != index {
+                    return [index, i]
+                }
+            }
+            dict[num] = i
+        }
+        return [0]
+    }
+}
 
+let solution = Solution()
+// 1. Two Sum Test Cases
+assert(solution.twoSum([2,7,11,15], 9) == [0,1])
+assert(solution.twoSum([3,2,4], 6) == [1,2])
+assert(solution.twoSum([3,3], 6) == [0,1])
